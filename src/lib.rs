781263eb6a94de4f8e5fdd7f0248bc643da23a02
H2Sxxa <h2sxxa0w0@gmail.com>
2024-08-16 09:30:56 +0800

    ✨ Virtual Mask

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,8 @@ pub mod vpn;
 #[cfg(test)]
 mod test {
 
+    use std::i32;
+
     use crate::{auth::AuthorizationStore, vpn::EnlinkVpn};
 
     #[tokio::test]
@@ -15,5 +17,20 @@ mod test {
         vpn.authorize().await.unwrap();
         println!("{:?}", vpn.is_authorize_ok().await);
         println!("{:?}", vpn.virtual_address().await);
+        println!("{:?}", vpn.virtual_mask().await);
+    }
+
+    #[test]
+    fn bin_eva() {
+        let v: i32 = 267;
+        println!("{}", v);
+        println!("{:?}", v.to_ne_bytes());
+        println!(
+            "{} {} {} {}",
+            (v >> 24) & 255,
+            (v >> 16) & 255,
+            (v >> 8) & 255,
+            v & 255
+        );
     }
 }
--- src/vpn.rs
+++ src/vpn.rs
@@ -39,20 +39,20 @@ impl<A: Authorization> EnlinkVpn<A> {
     }
 
     pub async fn authorize(&self) -> tokio::io::Result<()> {
-        let mut stream = self.stream.lock().await;
+        let mut guard = self.stream.lock().await;
         let token = self.authorization.token();
         let bytes_token = token.as_bytes();
         let user = self.authorization.user();
         let bytes_user = user.as_bytes();
         // Version
-        stream.write_u8(1).await?;
+        guard.write_u8(1).await?;
         // Protocal
-        stream.write_u8(1).await?;
+        guard.write_u8(1).await?;
         // Length
-        stream
+        guard
             .write_u16(19 + bytes_user.len() as u16 + bytes_token.len() as u16)
             .await?;
-        stream
+        guard
             .write(&[
                 0, 0, 0, 0, // Zero
                 1, 0, 0, 0, // ELK_METHOD_STUN
@@ -60,85 +60,110 @@ impl<A: Authorization> EnlinkVpn<A> {
             ])
             .await?;
         // User
-        stream.write_u8(bytes_user.len() as u8).await?;
-        stream.write(bytes_user).await?;
+        guard.write_u8(bytes_user.len() as u8).await?;
+        guard.write(bytes_user).await?;
         // ELK_OPT_SESSID
-        stream.write(&[2, 0]).await?;
+        guard.write(&[2, 0]).await?;
 
         // Token
-        stream.write_u8(bytes_token.len() as u8).await?;
-        stream.write(bytes_token).await?;
+        guard.write_u8(bytes_token.len() as u8).await?;
+        guard.write(bytes_token).await?;
         // -1 & 0xff
 
-        stream.write_u8(255).await?;
+        guard.write_u8(255).await?;
 
-        stream.flush().await?;
+        guard.flush().await?;
         Ok(())
     }
 
     pub async fn heartbeat(&self) -> tokio::io::Result<()> {
-        let mut stream = self.stream.lock().await;
+        let mut guard = self.stream.lock().await;
 
-        stream.write(&[1, 1, 0, 12, 0, 0, 0, 0, 3, 0, 0, 0]).await?;
-        stream.flush().await?;
+        guard.write(&[1, 1, 0, 12, 0, 0, 0, 0, 3, 0, 0, 0]).await?;
+        guard.flush().await?;
         Ok(())
     }
 
     pub async fn write_tcp(&self, data: &[u8], read: i16) -> tokio::io::Result<()> {
-        let mut stream = self.stream.lock().await;
+        let mut guard = self.stream.lock().await;
         // Custom header
-        stream.write(&[1, 4]).await?;
+        guard.write(&[1, 4]).await?;
         // Length
-        stream.write_i16(read + 12).await?;
+        guard.write_i16(read + 12).await?;
         // XID
-        stream.write(&[0, 0, 0, 0]).await?;
-        stream.write_i32(1).await?;
+        guard.write(&[0, 0, 0, 0]).await?;
+        guard.write_i32(1).await?;
         // Data
-        stream.write(data).await?;
-        stream.write_u8(0).await?;
-        stream.write_i16(read).await?;
-        stream.flush().await?;
+        guard.write(data).await?;
+        guard.write_u8(0).await?;
+        guard.write_i16(read).await?;
+        guard.flush().await?;
         Ok(())
     }
 
     // Read
     pub async fn is_authorize_ok(&self) -> tokio::io::Result<bool> {
-        let mut stream = self.stream.lock().await;
+        let mut guard = self.stream.lock().await;
         // Skip 10
-        stream.read_exact(&mut [0u8; 10]).await?;
+        guard.read_exact(&mut [0u8; 10]).await?;
 
         let mut status = vec![0, 0];
-        stream.read_exact(&mut status).await?;
+        guard.read_exact(&mut status).await?;
         Ok(status[0] == 0 && status[1] == 0)
     }
 
-    pub async fn virtual_address(&self) -> tokio::io::Result<Vec<u32>> {
-        let mut status = [0u8; 3];
-        self.stream.lock().await.read_exact(&mut status).await?;
-        println!("{:?}", status);
+    pub async fn virtual_address(&self) -> tokio::io::Result<Vec<u8>> {
+        let mut guard = self.stream.lock().await;
 
+        let mut status = [0u8; 3];
+        guard.read_exact(&mut status).await?;
         if status[0] == 11 && status[1] == 0 && status[2] == 4 {
-            let mut virtual_address = [0u8; 16];
-            self.stream
-                .lock()
-                .await
-                .read_exact(&mut virtual_address)
-                .await?;
-            println!("{:?}", virtual_address);
-            return Ok((0..4)
-                .map(|index| {
-                    u32::from_le_bytes([
-                        virtual_address[index],
-                        virtual_address[index + 1],
-                        virtual_address[index + 2],
-                        virtual_address[index + 3],
-                    ])
-                })
-                .collect());
+            let mut data = vec![0u8; 4];
+            guard.read_exact(&mut data).await?;
+            Ok(data.iter().map(|val| val & 255).collect())
+        } else {
+            Err(tokio::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid Received Data Header",
+            ))
         }
-        Err(tokio::io::Error::new(
-            std::io::ErrorKind::InvalidData,
-            "Invalid Received Data",
-        ))
+    }
+
+    pub async fn virtual_mask(&self) -> tokio::io::Result<usize> {
+        let mut guard = self.stream.lock().await;
+
+        let mut status = [0u8; 3];
+        guard.read_exact(&mut status).await?;
+        if status[0] == 12 && status[1] == 0 && status[2] == 4 {
+            let mut len = 0;
+            let mut a = 0;
+            let mut b = 0;
+            let mut data = vec![0u8; 4];
+            guard.read_exact(&mut data).await?;
+            data.iter().for_each(|val| {
+                let val = val & 255;
+                let binary = format!("{val:b}");
+                loop {
+                    let pos = binary
+                        .chars()
+                        .enumerate()
+                        .position(|(pos, char)| pos >= b && char == '1');
+                    if let Some(pos) = pos {
+                        a = pos + 1;
+                        b += 1;
+                    } else {
+                        break;
+                    }
+                }
+                len += b;
+            });
+
+            Ok(len)
+        } else {
+            Err(tokio::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "Invalid Received Data Header",
+            ))
+        }
     }
 }
